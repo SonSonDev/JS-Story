ee68516fc32720ac1540cfd737f15d713037dd05
ShonhTan <thanh-son-vincent.pham@hetic.net>
2018-06-05 19:10:16 +0200

    Finished story + add musics + homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,64 @@
-var imgFolder = "./img/"
+var imgFolder = "./img/";
+var musicFolder = "./music/";
 var data;
 
-function ajaxRequest() {
-    var request = new XMLHttpRequest();
-    request.open("POST", "scripts/story2.json", true);
-    request.onreadystatechange = function() {
-        if (request.readyState != 4 || request.status != 200) return;
-        data = JSON.parse(request.responseText);
-    };
-    console.log(request.readyState);
-    request.send();
-};
 
-function aaaaaajax(){
+//requete ajax
+function ajaxRequest(){
     fetch("./scripts/story2.json")
     .then(function(res){
         return res.json()
-    }).then(function(res){
-        data = res
+    }).then(function(story){
+        data = story
     })
 
 }
 
-aaaaaajax();
+//ecran d'accueil
+var home = {
+    screen: document.querySelector(".home"),
+    button: document.querySelector(".home_playButton")
+}
 
-var ui = {
-    startBtn: document.querySelector(".startBtn")
-};
+home.button.addEventListener('click', function(){
+    home.screen.classList.add('hidden');
+})
 
+
+//element du DOM à modifier
 var storyUi = {
+    music: document.querySelector(".gameMusic"),
     picture: document.querySelector(".gameImg"),
     message: document.querySelector(".messageBox"),
-    choiceSection: document.querySelector(".choiceSection")
+    choiceSection: document.querySelector(".choiceSection"),
 };
 
+//progression de l'histoire
 var progression = {
     route: "Intro",
     step: 0
 };
 
+//fonction executée lorsqu'une réponse est choisie. Permet de choisir une route
+function chosenRoute(route) {
+    progression.step = 0
+    progression.route = route;
+    storyUi.message.innerHTML = "";
+    storyUi.choiceSection.innerHTML = "";
+    var newP = document.createElement('p');
+    newP.textContent = data[progression.route].step[progression.step].message;
+    storyUi.message.appendChild(newP);
+    storyUi.message.innerHTML += '<img src="img/messageboxI.png" alt="messageboxI">';
+    storyUi.message.classList.add('clickable')
+    if(data[progression.route].step[progression.step].picture){
+        storyUi.picture.src= imgFolder + data[progression.route].step[progression.step].picture;
+        
+    }
+    
+    musicAndPicture();
+}
+
+//fonction qui permet d'avancer dans le récit
 function next() {
     if (progression.step >= data[progression.route].step.length - 1) {
         return;
@@ -49,11 +69,18 @@ function next() {
     var newP = document.createElement('p');
     newP.textContent = data[progression.route].step[progression.step].message;
     storyUi.message.appendChild(newP);
+
     if(data[progression.route].step[progression.step].picture){
         storyUi.picture.src= imgFolder + data[progression.route].step[progression.step].picture;
     }
+    musicAndPicture();
     if (progression.step >= data[progression.route].step.length - 1) {
         getChoices();
+        storyUi.message.classList.remove('clickable')
+        
+    } else {
+        storyUi.message.innerHTML += '<img src="img/messageboxI.png" alt="messageboxI">';
+        
     }
     
 }
@@ -69,26 +96,25 @@ function getChoices() {
     }
 }
 
-function chosenRoute(route) {
-    progression.step = 0
-    progression.route = route;
-    storyUi.message.innerHTML = "";
-    storyUi.choiceSection.innerHTML = "";
-    var newP = document.createElement('p');
-    newP.textContent = data[progression.route].step[progression.step].message;
-    storyUi.message.appendChild(newP);
+function musicAndPicture(){
     if(data[progression.route].step[progression.step].picture){
         storyUi.picture.src= imgFolder + data[progression.route].step[progression.step].picture;
     }
+    if(data[progression.route].step[progression.step].music){
+       
+        if(data[progression.route].step[progression.step].music.function==="play"){
+            storyUi.music.src= musicFolder + data[progression.route].step[progression.step].music.src;
+            storyUi.music.play();
+        } else if(data[progression.route].step[progression.step].music.function==="pause"){
+            storyUi.music.pause();
+            storyUi.music.currentTime=0;
+        }
+    }
 }
-
-
-ui.startBtn.addEventListener('click', function() {
-    chosenRoute("Intro")
-})
 
 storyUi.message.addEventListener('click', function() {
     next();
 })
 
-aaaaaajax();
+
+ajaxRequest();
